0c3257d81479b619032759e75d6952ee88e6998f
VibeFJ <raziel.rosfo.1904@gmail.com>
2022-10-28 00:07:31 -0500

    Proyecto Go rutines

---
+++ IAMC.go
@@ -0,0 +1,79 @@
+// Una _goroutine_ es un hilo o thread de ejecución ligero.
+
+package main
+
+import "fmt"
+
+var Nombre string
+
+func Terminado() {
+	fmt.Println("     *       *******    *******       *      ***    **      *     ")
+	fmt.Println("    * *      *     *      ***        * *     ** **  **     * *    ")
+	fmt.Println("   *****     *******      ***       *****    ** *** **    *****   ")
+	fmt.Println("  *     *    *   *        ***      *     *   **  ** **   *     *  ")
+	fmt.Println(" *       *   *     *    *******   *       *  **    ***  *       * ")
+}
+
+func A() {
+	Nombre = Nombre + "A"
+
+	// fmt.Println("     *     ")
+	// fmt.Println("    * *    ")
+	// fmt.Println("   *****   ")
+	// fmt.Println("  *     *  ")
+	// fmt.Println(" *       * ")
+}
+
+func R() {
+	Nombre = Nombre + "R"
+
+	// fmt.Println(" *******  ")
+	// fmt.Println(" *     *  ")
+	// fmt.Println(" *******  ")
+	// fmt.Println(" *   *    ")
+	// fmt.Println(" *     *  ")
+}
+
+func I() {
+	Nombre = Nombre + "I"
+
+	// fmt.Println("  *******  ")
+	// fmt.Println("    ***    ")
+	// fmt.Println("    ***    ")
+	// fmt.Println("    ***    ")
+	// fmt.Println("  *******  ")
+}
+
+func N() {
+	Nombre = Nombre + "N"
+
+	// fmt.Println(" ***    ** ")
+	// fmt.Println(" ** **  ** ")
+	// fmt.Println(" ** *** ** ")
+	// fmt.Println(" **  ** ** ")
+	// fmt.Println(" **    *** ")
+}
+
+func main() {
+
+	for i := 1; i > 0; i++ {
+
+		if Nombre == "ARIANA" {
+			i = -1
+			Terminado()
+		} else {
+
+			Nombre = ""
+			go R()
+			go A()
+			go I()
+			go A()
+			go N()
+			go A()
+
+			var input string
+			fmt.Scanln(&input)
+			fmt.Println(Nombre, i)
+		}
+	}
+}
